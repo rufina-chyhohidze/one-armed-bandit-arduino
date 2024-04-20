e6ea31fea7d7ea426b7b8e29ab63f77f8ea61cbd
Rufina Chyhohidze <rufina.chyhohidze@student.kdg.be>
2024-04-20 10:02:12 +0000

    Update file led.h

--- Libraries/lib/led/led.h
+++ Libraries/lib/led/led.h
@@ -1,18 +1,18 @@
 void enabledLed(int);
 
-void enableMultipleLeds(uint8_t); 
+void enableMultipleLeds(uint8_t leds); 
 
 void enableAllLeds ();
 
 void lightUpLed(int); 
 
-void lightUpMultipleLeds (uint8_t);
+void lightUpMultipleLeds (uint8_t leds);
 
 void lightUpAllLeds (); 
 
 void lightDownLed(int); 
 
-void lightDownMultipleLeds (uint8_t);
+void lightDownMultipleLeds (uint8_t leds);
 
 void lightDownAllLeds (); 
 
