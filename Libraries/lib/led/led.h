dc1700b0104844b48676b61d8b8495dd7a13f239
Rufina Chyhohidze <rufina.chyhohidze@student.kdg.be>
2024-04-20 11:03:11 +0000

    chenges in function

--- Libraries/lib/led/led.h
+++ Libraries/lib/led/led.h
@@ -1,19 +1,25 @@
 void enabledLed(int);
 
-void enableMultipleLeds(uint8_t ledNumber); 
+void enableMultipleLeds(uint8_t leds); 
 
 void enableAllLeds ();
 
 void lightUpLed(int); 
 
-void lightUpMultipleLeds (uint8_t ledNumber);
+void lightUpMultipleLeds (uint8_t leds);
 
 void lightUpAllLeds (); 
 
 void lightDownLed(int); 
 
-void lightDownMultipleLeds (uint8_t ledNumber);
+void lightDownMultipleLeds (uint8_t leds);
 
 void lightDownAllLeds (); 
 
 void lightToggleOneLed(int);
+
+void dimLed(int ledNumber, int percentage, int duration);
+
+void fadeInLed(int ledNumber, int duration);
+
+void fadeOutLed(int ledNumber, int duration);
