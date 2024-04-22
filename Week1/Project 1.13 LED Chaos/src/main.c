6a4aa7fe51f206d74700caad69360c78fcfa6c70
Rufina Chyhohidze <rufina.chyhohidze@student.kdg.be>
2024-04-22 21:47:05 +0200

    Bug (unfixible)

---
+++ Week1/Project 1.13 LED Chaos/src/main.c
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include <stdlib.h>  // For rand()
+#include <led.h>    // Include the LED header file
+#define __DELAY_BACKWARD_COMPATIBLE__  // Allows variables in _delay_ms
+#include <util/delay.h>  // For delay functions
+#include <avr/io.h>
+
+
+int main(){
+ DDRB = 0b000000100;
+ PORTB = 0b00000000;
+ return 0;
+}
