a887f55b1ea37caa5d8c56c46acebfa926ca1ef0
Rufina Chyhohidze <rufina.chyhohidze@student.kdg.be>
2024-04-26 15:02:16 +0200

    buttonPushed

--- Libraries/lib/buttons/buttons.c
+++ Libraries/lib/buttons/buttons.c
@@ -5,7 +5,11 @@ void enableButton( int button ){
     PORTC |= _BV( button ); 
 } ;
 int buttonPushed( int button ){
-
+    if (bit_is_clear(PINC, button)) { // Assuming buttons are connected to Port C
+        return 1; // Button is pressed (because pull-up resistor makes it high when not pressed)
+    } else {
+        return 0; // Button is not pressed
+    }
 };
 int buttonReleased( int button ){
 
---
+++ Week2/Project 2.7.1 Button press and release/src/main.c
@@ -0,0 +1,23 @@
+#include <util/delay.h> 
+#include <avr/io.h> 
+#include <usart.h> 
+#include<buttons.h>
+
+
+int main() 
+{ 
+    initUSART(); 
+    // Enable button 1 (which is on PC1)
+    enableButton(PC1); 
+
+    while (1) 
+    {  
+        if (buttonPushed(PC1)==0) {
+            printf(" - Button 1 pressed!\n"); 
+        } else { 
+            printf(" - Button 1 NOT pressed!!\n"); 
+        }
+        _delay_ms(1000); 
+    } 
+    return 0; 
+}
