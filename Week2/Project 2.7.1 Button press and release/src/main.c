28508a390a278b1bbc36a8ff512212c92d666e73
Rufina Chyhohidze <rufina.chyhohidze@student.kdg.be>
2024-04-26 15:20:45 +0200

    added break point.

--- Week2/Project 2.7.1 Button press and release/src/main.c
+++ Week2/Project 2.7.1 Button press and release/src/main.c
@@ -6,6 +6,7 @@
 
 int main() 
 { 
+  
     initUSART(); 
     // Enable button 1 (which is on PC1)
     enableButton(PC1); 
@@ -14,10 +15,19 @@ int main()
     {  
         if (buttonPushed(PC1)==1) {
             printf(" - Button 1 pressed!\n"); 
+            break;
         } else { 
             printf(" - Button 1 NOT pressed!!\n"); 
+      
         }
         _delay_ms(1000); 
-    } 
+        if (buttonReleased(PC1)==1) { // Check if button on PC1 is released
+            printf("Button on PC1 is released!\n");
+        } else {
+            printf("Button on PC1 is still pressed.\n");
+        
+        }
+        _delay_ms(1000); 
+    }
     return 0; 
 }
