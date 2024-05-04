4fbde7de6ad7ef5349293a9efa96c9dd34fd3a25
Rufina Chyhohidze <rufina.chyhohidze@student.kdg.be>
2024-05-04 13:11:15 +0200

    TESTING with a simple main HELL (O)

--- Week3/Project Alphabet on Display/src/main.c
+++ Week3/Project Alphabet on Display/src/main.c
@@ -3,17 +3,11 @@
 #include "display.h"
 
 int main() {
-    initDisplay(); // Initialize the display
+    initDisplay(); 
 
     while (1) {
-        // Display a test string using writeString
-        writeString("OPEN");
-        _delay_ms(2000); // Hold the display for 2 seconds
-
-        // Display a different string
-        writeString("AIRO");
-        _delay_ms(2000); // Hold the display for 2 seconds
+        writeString("HELLO"); //ACTUALLY LIFE GIVES ME SIGNS AND PRINT HELL INSTEAD LMAO
+        _delay_ms(0.100); // Hold the display for 2 secondss
     }
-
     return 0;
 }
