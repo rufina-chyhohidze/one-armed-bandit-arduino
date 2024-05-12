0f7bcbf091a8a97887a7a5bed22d6e9323471c6e
Rufina Chyhohidze <rufina.chyhohidze@student.kdg.be>
2024-05-12 16:50:21 +0200

    exercise 4(not completed yet)

--- Week2/Project Simon Say/src/main.c
+++ Week2/Project Simon Say/src/main.c
@@ -12,6 +12,11 @@ volatile uint8_t counter = 0; // Counter for generating random seed
 
 #define PUZZLE_LENGTH 10 // array of 10
 uint8_t puzzle[PUZZLE_LENGTH]; //  store the random puzzle in array
+#define DELAY 300
+
+#define LED1 PB2
+#define LED2 PB3
+#define LED3 PB3
 
 void enablePinChangeInterrupt(int pinGroup, int pin) {
     if (pinGroup == 1) { 
@@ -26,7 +31,7 @@ ISR(PCINT1_vect) {
     }
 }
 
-// Function to generate the random puzzle
+// generate the random puzzle
 void generatePuzzle(uint8_t *puzzle, uint8_t length) {
     for (uint8_t i = 0; i < length; i++) {
         puzzle[i] = rand() % 3; 
@@ -45,28 +50,53 @@ void printPuzzle(uint8_t *puzzle, uint8_t length) {
     printf("]\n");
 }
 
+void playPuzzle(uint8_t *puzzle,uint8_t length){
+  for (uint8_t i =0; i<length;i++){
+    switch (puzzle[i])
+    {
+    case 0:
+      lightUpLed(LED1);
+      break;
+    case 1:
+     lightUpLed(LED2);
+     break;
+    case 2:
+     lightUpLed(LED3);
+     break;
+      default:
+      break;
+    }
+    _delay_ms(DELAY);
+    lightDownAllLeds (); //turn off all leds
+  }
+
+}
+
 int main() 
 { 
+  
     initUSART(); 
     DDRB |= _BV(PB5); 
     enablePinChangeInterrupt(1, PC1); 
+    
     sei(); 
     
     while (1) 
     {
         if (ledFlashing) {
             PORTB ^= _BV(PB5); 
-            _delay_ms(200);
+            _delay_ms(DELAY);
             counter++; 
         }
         
         if (buttonPressed) {  
             srand(counter);
-            
             generatePuzzle(puzzle, PUZZLE_LENGTH);
             
             printf("Random Puzzle: ");
             printPuzzle(puzzle, PUZZLE_LENGTH);
+
+             playPuzzle(puzzle, PUZZLE_LENGTH);
             
             buttonPressed = 0; 
         }
