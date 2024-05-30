9fc285e4e0b82a1a76690520955df89b7b0d913f
Rufina Chyhohidze <rufina.chyhohidze@student.kdg.be>
2024-05-30 11:16:30 +0200

    innit

--- Libraries/lib/potentio/potentio.c
+++ Libraries/lib/potentio/potentio.c
@@ -1,23 +1,23 @@
 #include "potentio.h"
+#include <avr/io.h>
+ 
 void initADC()
 {
-    ADMUX |= (1 << REFS0); // Set up the reference voltage. We choose 5V as the reference. 
-    ADMUX &= ~(1 << MUX3 ) & ~(1 << MUX2 ) & ~(1 << MUX1 ) & ~(1 << MUX0 ); 
-                                                          //Set MUX0-3 to zero to read analog input from PC0 
+    ADMUX |= (1 << REFS0); // Set up the reference voltage. We choose 5V as the reference.
+    ADMUX &= ~(1 << MUX3 ) & ~(1 << MUX2 ) & ~(1 << MUX1 ) & ~(1 << MUX0 );
+                                                          //Set MUX0-3 to zero to read analog input from PC0
                                                           //Default is 0000 so this setting is not really necessary      
     ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // Determine the sample rate by setting the division factor to 128.
     ADCSRA |= (1 << ADEN); // Enable the ADC
-    ADCSRA |= (1 << ADATE); // Enable ADC Auto Triggering
-    ADCSRB = 0; // Set ADC Auto Trigger Source to Free Running Mode (default)
-    ADCSRA |= (1 << ADSC); // Start the analog-to-digital conversion
+    
+ 
 }
-//possibly then include: 
+//possibly then include:
 // uint16_t value = 0;
 // value = ADC; //to read the result immediately
-
+ 
 int potentioMeterNumber() {
  
-    initADC();
  
     ADCSRA |= ( 1 << ADSC );
     loop_until_bit_is_clear( ADCSRA, ADSC );    //Wait until the conversion is completed
