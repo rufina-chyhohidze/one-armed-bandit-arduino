6bfc5ba6ccac56f28c9ac450c1a426b6cab04f77
Rufina Chyhohidze <rufina.chyhohidze@student.kdg.be>
2024-05-30 12:30:16 +0200

    added potentio for a random generated numbers + comments

--- Week 5 - 6 Individual Arduino Project One armed bandit/src/main.c
+++ Week 5 - 6 Individual Arduino Project One armed bandit/src/main.c
@@ -1,13 +1,12 @@
-//TODO:IMPLEMENT POTENTIOMETER 
 //TODO:IMPLEMENT CALLOC AND MALLOC 
 //TODO:EXPAND THE README FILE
 //TODO:Use of pointers and dynamic memory allocation
 //Parameter “by value” and “by reference”
 
 //at first including each library,that i will use in this project.
+#define __DELAY_BACKWARD_COMPATIBLE__
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#define __DELAY_BACKWARD_COMPATIBLE__
 #include <util/delay.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,26 +18,26 @@
 #include <usart.h>  
 #include <string.h>
 #include <time.h>
-
+ 
 //defining leds,buttons...
 #define LED1 PB2
 #define LED2 PB3
 #define LED3 PB4
 #define LED4 PB5
 #define BUZZER_PIN PD3
-
+ 
 #define BUTTON1 PC1
 #define BUTTON2 PC2
 #define BUTTON3 PC3
-
-//using macro's to express the constant values 
-#define START_COINS 5 //i decreased start coins, to play faster for a moment.
-#define MAX_COINS 5
+ 
+//using macro's to express the constant values
+#define START_COINS 10 //i decreased start coins, to play faster for a moment.
+#define MAX_COINS 10
 #define WIN_AMOUNT_2 5
 #define WIN_AMOUNT_3 50
 #define WIN_AMOUNT_4 500
-
-//DEFINING NOTES 
+ 
+//DEFINING NOTES
 #define C4  261.63
 #define D4  293.66
 #define E4  329.63
@@ -51,14 +50,13 @@
 #define E5  659.25
 #define F5  698.46
 #define G5  783.99
-#define DURATION 150 
-
+#define DURATION 150
 #define MAX_GAME_STATES 10
-
+ 
 int coins = START_COINS;
-
-volatile unsigned long gameTimeSeconds = 0; 
-
+ 
+volatile unsigned long gameTimeSeconds = 0;
+ 
 typedef struct {
     int sequence;
     int coins;
@@ -66,57 +64,59 @@ typedef struct {
     int displayValues[4];
     int winAmount;
 } GameState;
-
+ 
 const int ledPins[] = {LED1, LED2, LED3, LED4};
 const int buttonPins[] = {BUTTON1, BUTTON2, BUTTON3};
-
-
+ 
+ 
 void initTimer() {
     TCCR0B |= (1 << CS02) | (1 << CS00); // sets prescaler 1024, (1024 / 16,000,000) seconds,a 16 MHz clock frequency.
     TIMSK0 |= (1 << TOIE0); // enable overflow interrupt
-    sei(); 
+    sei();
 }
-
 /*Timer 0 will count up from 0 to its maximum value
- (255 for an 8-bit timer)
+(255 for an 8-bit timer)
   and then overflow back to 0.
- Each time it overflows, this ISR is triggered*/
-
+Each time it overflows, this ISR is triggered*/
 ISR(TIMER0_OVF_vect) {
     gameTimeSeconds++;
 }
-
-//this function 
+ 
+//function to dsiplay calculated gameTimeSeconds in serial monitor.
 void displayGameTime() {
+
     int seconds = gameTimeSeconds / 60;
     printf("\nTotal game time: %d seconds\n", seconds);
+ 
 }
-
+ 
+ 
 void victorySound() {
-  float  frequencies [] = {C4, E4, G4, C5, C5, G4, E4, C4, G4, A4, B4, C5, G4, E4, D4, C4}; // C D E C twice, followed by E F G once
-enableBuzzer (); 
-for ( int  note = 0 ; note < 8 ; note ++ ) 
-{ 
-  playTones ( frequencies [ note ], DURATION ); 
- _delay_ms(50); //wait 50 ms between the notes 
- } 
+ 
+  float frequencies [] = {C4, E4, G4, C5, C5, G4, E4, C4, G4, A4, B4, C5, G4, E4, D4, C4}; //array with C D E C twice, followed by E F G once
+  enableBuzzer (); 
+  for ( int  note = 0 ; note < 8 ; note ++ ) { //go through each note in array
+  playTones ( frequencies [ note ], DURATION ); //play current note, with defined duration of 150 ms
+  _delay_ms( 50 ); //wait 50 ms between the notes
+  }
+ 
 }
-
+ 
 void lossSound() {
-    float frequencies[] = {C5, B4, A4, G4, F4, E4, D4, C4}; // Descending pattern for loss sound
-    int numNotes = sizeof(frequencies) / sizeof(frequencies[0]);
+    float frequencies[] = {C5, B4, A4, G4, F4, E4, D4, C4}; //array for descending pattern for loss sound
+    int numNotes = sizeof(frequencies) / sizeof(frequencies[0]); //calculate the number of notes in array
     enableBuzzer();
-    for (int note = 0; note < numNotes; note++) {
-        playTones(frequencies[note], DURATION);
+    for (int note = 0; note < numNotes; note++) { //loop through each note in array
+        playTones(frequencies[note], DURATION); //play the current note with defined duration of 150 ms
         _delay_ms(50); // Wait 50 ms between the notes
     }
 }
-
-
+ 
+ 
 void initGame() {
     // I plan to initialize LEDs, buttons, etc. in this function
 }
-
+ 
 // Function to print rules of the game to the serial monitor,used 200ms of delay for better readability.
 void printGameRules() {
     printf("\nPlace your bet: Press the right button once to bet 1 coin per turn. LED displays light up, and a sound signifies the bet.\r\n");
@@ -132,52 +132,52 @@ void printGameRules() {
     printf("                        PRESS BUTTON 2 FOR 3 SLOTS DISPLAY.\n");
     _delay_ms(200);
     printf("                        PRESS BUTTON 3 FOR 4 SLOTS DISPLAY.\n");
-
+ 
 }
-
+ 
 //function for display coins on serail monitor and display
 void displayCoins(int coins) {
-    char coinsStr[5];
-    snprintf(coinsStr, sizeof(coinsStr), "%4d", coins);
-    for(int i = 0; i < 4; i++) {
-        writeCharToSegment(i, coinsStr[i]);
+ 
+    char coinsStr[5]; //store the coin count as a string.
+    snprintf(coinsStr, sizeof(coinsStr), "%4d", coins);//convert the integer coin count to a string with a width of 4 characters
+ 
+    for(int i = 0; i < 4; i++) { //loop through each character in a string
+        writeCharToSegment(i, coinsStr[i]); //write each char to corresponding segment
     }
 }
-
+ 
 //function to generate the random number and show it on the display
 void displayRandomNumbers(int slotCount) {
-    int numbers[4];
+    int numbers[4]; //arrray for storing rand numbers 
     for (int i = 0; i < slotCount; i++) {
-        numbers[i] = rand() % 10; // Generate random number between 0 and 9
+        numbers[i] = rand() % 10; // Generate random number between 0 and 9 for each slot
     }
-
-    // Display all numbers simultaneously
+ 
+    // Display all numbers simultaneously on segment 
     for (int i = 0; i < slotCount; i++) {
-        writeNumberToSegment(i, numbers[i]);
+        writeNumberToSegment(i, numbers[i]);//write each random number on each segment of the display
     }
-
+ 
     // Check if all numbers are the same
-    int win = 1;
-    for (int i = 1; i < slotCount; i++) {
-        if (numbers[i] != numbers[0]) {
-            win = 0;
-            break;
+    int win = 1; //flag if all numbers the same
+    for (int i = 1; i < slotCount; i++) { 
+        if (numbers[i] != numbers[0]) { //compare each number with 1st num
+            win = 0; //if any number is different,set flag to 0.
+            break; //exit the loop,as we found a mismatch.
         }
     }
-    if (win) {
-        int winAmount = (slotCount == 2) ? WIN_AMOUNT_2 : (slotCount == 3) ? WIN_AMOUNT_3 : WIN_AMOUNT_4;
-        coins += winAmount;
+    if (win) { //if win still 1 ,all numbers the are same 
+        //determine the amount of win,based on the number of slots
+        int winAmount = (slotCount == 2) ? WIN_AMOUNT_2 : (slotCount == 3) ? WIN_AMOUNT_3 : WIN_AMOUNT_4;//if slotCount ==2 true, assign win_amount_2 to winAmount,if false assign win_amount_3 and if it's false assign win_amount_4.
+        coins += winAmount; //add the win amount to player's coins
         printf("Congratulations! You won %d coins!\n", winAmount);
         //victorySound();
     }
-
     displayCoins(coins);
 }
-
-
-//function to chech if the game is over 
+//function to chech if the game is over
 void checkGameOver() {
-    if (coins <= 0) {
+    if (coins <= 0) { //if player's amount of coind less than 0, print loss statement 
         printf("Game over! You have run out of coins.\n");
         lossSound();
         _delay_ms(2000);
@@ -190,91 +190,68 @@ void checkGameOver() {
             writeCharToSegment(2,'s');
             writeCharToSegment(3,'s');
         }
-    } else if (coins >= MAX_COINS) {
+    } else if (coins >= MAX_COINS) { //if player's coins reached max coins number, print win statement.
         printf("Congratulations! Bank break! You have reached the maximum coins.\n");
         _delay_ms(1000);
         printf("Press the RESTART button to try your luck again!");
          displayGameTime(); // display total playing time in seconds in the end of the game.
-        victorySound(); 
+        victorySound(); //play sound of victory
         while (1) {
            //shows winning 7777!!!
             writeNumber(7777);
            
-            
+           
         }
     }
 }
-
+ 
 void checkWin(int numbers[], int slotCount) {
-    int winAmount = 0;
+    int winAmount = 0; //variable to store winning amount 
     int sameNumber = 1; // Flag to check if all numbers are the same
     int targetNumber = numbers[0]; // Store the first number as the target number
-
+ 
     // Checks if all numbers are the same
-    for (int i = 1; i < slotCount; i++) {
-        if (numbers[i] != targetNumber) {
-            sameNumber = 0; // Set the flag to false if any number is different
-            break;
+    for (int i = 1; i < slotCount; i++) { //loop through remaining numbers 
+        if (numbers[i] != targetNumber) { //if any number is diff from the target number
+            sameNumber = 0; // Set the flag to false then
+            break; //exit loop bc we have false.
         }
     }
-
+ 
     if (sameNumber) { // If all numbers are the same
-        winAmount = (slotCount == 2) ? WIN_AMOUNT_2 : (slotCount == 3) ? WIN_AMOUNT_3 : WIN_AMOUNT_4;
+        winAmount = (slotCount == 2) ? WIN_AMOUNT_2 : (slotCount == 3) ? WIN_AMOUNT_3 : WIN_AMOUNT_4; //if slotCount ==2 true, assign win_amount_2 to winAmount,if false assign win_amount_3 and if it's false assign win_amount_4.
         coins += winAmount; // Add the win amount to coins
         printf("Congratulations! You won %d coins!\n", winAmount);
     } else {
         printf("Better luck next time!\n");
     }
-
+ 
     displayCoins(coins); // Update coin display
 }
 
- /*int bet = 1;
-  void makeBet(){
-          uint16_t adcValue = ADC;
-    int displayValue = ((adcValue * 19)/1023)+ 1  ; // Scale to range 1-10
-    writeNumber(displayValue); // Display the scaled value
-    printf("Your bet is: %d\n", displayValue); // Print the bet value
-    
-    // Check if the display value exceeds the maximum bet
-    if (displayValue > coins) {
-        printf("Insufficient coins for this bet! Setting the maximum bet.\n");
-        displayValue = coins; // Set the bet amount to the maximum coins
-    }
-    
-    bet = displayValue; // Update the bet value
-    coins -= bet; // Subtract the bet from current coins
-
-    // Display the current coins after the bet
-    displayCoins(coins);
-        _delay_ms(0.100);
-    };
-    */
  
 int main() {
     // Initialization
-    initADC();
-    initUSART();
+    initADC();//initialise potentio
+    initUSART(); // communication with my PC over the serial port (USB cable).
     initGame();
-    initDisplay();
+    initDisplay();//initialise display
     initTimer(); //enables timer routine and ISR
-    srand(time(NULL));
-    writeWelcomeToTheUserOnDisplay();
-    DDRB |= _BV(PB2); 
-    
+    int value = potentioMeterNumber(); //read value from potentio,and use it for random generated numbers.
+    srand(value); ////read value from potentio,and use it for random generated numbers.
+    writeWelcomeToTheUserOnDisplay(); // running lines of welcoming (in display.c)
+    //DDRB |= _BV(PB2);
    
     // Print game rules
     printf("----------------------------WELCOME TO ONE ARMED BANDIT!----------------\n ");
     _delay_ms(900);
     printf("-----------------------------------------------------------------------\n ");
     _delay_ms(900);
-    printf("---$_$_$_$_$_$_$_$_$_$_$_$_$_$ RULES TO PLAY: _$_$_$_$_$_$_$_$_$_$_$_$\n ");
+    printf("---$_$_$_$_$_$_$_$_$_$_$_$_$_$ RULES TO PLAY: _$_$_$_$_$_$_$_$_$_$_$_$_$_$---\n ");
     _delay_ms(900);
     printGameRules();
-
-    while (1) {
-        
-        
+    
+    while (1) {       
         if (bit_is_clear(PINC, BUTTON1)) {
             printf("Your choice is: 2 slots display.\n");
             //makeBet();
@@ -282,46 +259,50 @@ int main() {
             //coins-= bet; // substract one coin for the bet
             displayCoins(coins); // Update coin display
             printf("We are starting the game! Your current bank is: %d coins\n", coins);
-            rollOnDisplay();
+            rollOnDisplay(); //imitates the roll
            
-            
-
+           
+ 
             //this loop makes 2 leds blinks twice,and then it generate numbers.
             for(int i=0;i<2;i++){
-            lightUpMultipleLeds(0b0011);
+            lightUpMultipleLeds(0b0011); //set 2 leds on
             _delay_ms(200);
             lightDownAllLeds();
             _delay_ms(200);
            
             }
-
-            // Generate and display random numbers for 2 slots
+ 
+            // Generate and display random numbers for 2 slots(theyre always be different with use of potentio)
             int numbers[2];
             for (int i = 0; i < 2; i++) {
                 numbers[i] = rand() % 10; // Generate random number between 0 and 9
                 writeNumberToSegment(i, numbers[i]); // Display the number
-                _delay_ms(800); // it holds the number on display 500ms
-                
+                _delay_ms(800); // it holds the number on display 800ms
+               
             }
             checkWin(numbers, 2); // checks for win state, 2 same numbers in a ROW!
-            checkGameOver(); // it checks if the game is over 
+            checkGameOver(); // it checks if the game is over
+ 
         } else if (bit_is_clear(PINC, BUTTON2)) {
-            //similar pattern for 3 slots 
-             printf("Your choice is: 3 slots display.\n");
+ 
+            //similar pattern for 3 slots
+            printf("Your choice is: 3 slots display.\n");
             coins--; // substract one coin for the bet
             displayCoins(coins); // Update coin display
             printf("We are starting the game! Your current bank is: %d coins\n", coins);
             rollOnDisplay();
              
             //this loop blink 3 leds 3 times
-            for(int i=0;i<3;i++){
-            lightUpMultipleLeds(0b0111);
+            for(int i = 0; i < 3; i++){
+ 
+            lightUpMultipleLeds(0b0111);//set 3 leds on
             _delay_ms(150);
             lightDownAllLeds();
-            _delay_ms(150);
-            } 
-
-            // Generate and display random numbers for 2 slots
+            _delay_ms(150);//waits 150ms 
+ 
+            }
+ 
+            // Generate and display random numbers for 3 slots(theyre always be different with use of potentio)
             int numbers[3];
             for (int i = 0; i < 3; i++) {
                 numbers[i] = rand() % 10; // Generate random number between 0 and 9
@@ -330,23 +311,24 @@ int main() {
             }
             checkWin(numbers, 3);// 3 same numbers in a ROW!
             checkGameOver();  
-        
+       
+ 
         } else if (bit_is_clear(PINC, BUTTON3)) {
-               printf("Your choice is: 4 slots display.\n");
+            printf("Your choice is: 4 slots display.\n");
             coins--; // substract one coin for the bet
             displayCoins(coins); // Update coin display
             printf("We are starting the game! Your current bank is: %d coins\n", coins);
             rollOnDisplay(); //shows the animation of roll on display
-
-            //this loop blink 4 leds 4 times 
+ 
+            //this loop blink 4 leds 4 times
             for (int i=0;i<4;i++){
-            lightUpMultipleLeds(0b1111);
+            lightUpMultipleLeds(0b1111); //set 4 leds on
             _delay_ms(150);
             lightDownAllLeds();
             _delay_ms(150);
             }
-
-            // Generate and display random numbers for 2 slots
+ 
+            // Generate and display random numbers for 4 slots(theyre always be different with use of potentio)
             int numbers[4];
             for (int i = 0; i < 4; i++) {
                 numbers[i] = rand() % 10; // Generate random number between 0 and 9
@@ -356,11 +338,11 @@ int main() {
             checkWin(numbers, 4);// 4 same numbers in a ROW!
             checkGameOver();  
         }
-        writeNumber(coins); 
+        writeNumber(coins);
     }
-    
+   
     return 0;
-
+ 
 }
 
 
